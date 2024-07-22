d04e0978ec09f995f00155f0332f17b27e4a64de
romi0613 <pmin0613@naver.com>
2024-07-23 08:26:05 +0900

    3차 수정

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,11 @@
-export function menu() {
-    console.log(menu)
-}
+import { menu } from "./menu.js";
+import { port } from "./port.js";
+import { link } from "./link.js";
+import { smooth } from "./smooth.js";
+
+window.addEventListener("load", function(){
+    menu();
+    port();
+    link();
+    smooth();
+});
